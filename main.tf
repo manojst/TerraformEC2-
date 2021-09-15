044d12c4c4f09f428b11a2c45c0d62c22d96a298
Manojkumar Gnanasekaran <manojkumar.gnanasekaran@ust.com>
2021-09-15 14:02:33 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,10 +1,19 @@
-provider "aws" {
+/*provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIAXPYYNAEMNBA76HOX"
+  secret_key = "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"
+}*/
+
+provider "consul" {
+  address    = "${var.consul_address}:${var.consul_port}"
+  datacenter = var.consul_datacenter
   region     = "us-east-1"
   access_key = "AKIAXPYYNAEMNBA76HOX"
   secret_key = "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"
 }
 
-resource "aws_instance" "Myinstance" {
+
+/*resource "aws_instance" "Myinstance" {
    ami           = "${lookup(var.ami_id, var.region)}"
    instance_type = "t2.micro"
  }
@@ -12,5 +21,4 @@ resource "aws_instance" "Myinstance" {
  resource "aws_vpc" "Myvpc" {
     cidr_block = "10.0.0.0/22"
     tags = { Name = "terra_vpc" }
-}
-
+}*/
