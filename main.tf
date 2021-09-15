59c98add696943d0340141fff5a2048b6eb44c6f
Manojkumar Gnanasekaran <manojkumar.gnanasekaran@ust.com>
2021-09-15 13:26:28 +0000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,9 @@
+provider "aws" {
+  region     = "us-west-1"
+  access_key = "AKIAXPYYNAEMNBA76HOX"
+  secret_key = "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"
+}
+
 resource "aws_instance" "Myinstance" {
    ami           = "${lookup(var.ami_id, var.region)}"
    instance_type = "t2.micro"
